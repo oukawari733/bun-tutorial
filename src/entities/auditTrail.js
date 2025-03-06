4098a0b6c58e2997a60a2f54ec385b3f30a1fed9
maesha.taruna99 <maesha.taruna99@gmail.com>
2025-03-06 13:08:14 +0700

    Training development:
    - Remove Unused auditTrail.js

--- src/entities/auditTrail.js
+++
@@ -1,9 +0,0 @@
-import {integer, timestamp, varchar} from "drizzle-orm/pg-core";
-
-export const auditTrail = {
-    createdBy: varchar("created_by", { length: 100 }).notNull(),
-    createdDate: timestamp("created_date").defaultNow().notNull(),
-    modifiedBy: varchar("modified_by", { length: 100 }),
-    modifiedDate: timestamp("modified_date"),
-    version: integer("version").default(1).notNull(),
-};
